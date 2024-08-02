8c10761bc6d98fef3f213c9b8225a369829d1f7e
Owen Wexler <owenbwexler@gmail.com>
2024-08-02 01:34:28 -0400

    Try client:only=react directive to fix lack of client functionality in prod

--- src/redis/redis.ts
+++ src/redis/redis.ts
@@ -2,4 +2,10 @@ import Redis from 'ioredis';
 
 const redisConnection = import.meta.env.REDIS_CONNECTION;
 
-export default redisConnection && redisConnection !== '' ? new Redis(redisConnection) : new Redis();
+const redis = redisConnection && redisConnection !== '' ? new Redis(redisConnection) : new Redis();
+
+redis.on('connect', () => {
+  console.log('REDIS connection successful.')
+})
+
+export default redis;
